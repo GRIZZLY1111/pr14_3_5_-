d6fb338df439bbc650e2ad935b0e5fb08a7aafef
GRIZZLY1111 <msolihov28@gmail.com>
2025-03-04 12:07:45 +0500

    Add files via upload

---
+++ praktika14(3-5)_Солихов/praktika14(3-5)_Солихов/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace praktika14_3_5__Солихов
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Вывод = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.n = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Фамилия = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Имя = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Отчество = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Возраст = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Вес = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(16, 77);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(181, 35);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Показать очередь";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Вывод
+            // 
+            this.Вывод.AutoSize = true;
+            this.Вывод.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Вывод.Location = new System.Drawing.Point(12, 115);
+            this.Вывод.Name = "Вывод";
+            this.Вывод.Size = new System.Drawing.Size(70, 20);
+            this.Вывод.TabIndex = 7;
+            this.Вывод.Text = "Вывод:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(94, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // n
+            // 
+            this.n.AutoSize = true;
+            this.n.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.n.Location = new System.Drawing.Point(12, 27);
+            this.n.Name = "n";
+            this.n.Size = new System.Drawing.Size(98, 20);
+            this.n.TabIndex = 5;
+            this.n.Text = "Введите n";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(16, 139);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(309, 304);
+            this.listBox1.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(331, 78);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(289, 34);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Вывести информацию о людях";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Фамилия,
+            this.Имя,
+            this.Отчество,
+            this.Возраст,
+            this.Вес});
+            this.dataGridView1.Location = new System.Drawing.Point(331, 139);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(503, 304);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // Фамилия
+            // 
+            this.Фамилия.HeaderText = "Фамилия";
+            this.Фамилия.Name = "Фамилия";
+            // 
+            // Имя
+            // 
+            this.Имя.HeaderText = "Имя";
+            this.Имя.Name = "Имя";
+            // 
+            // Отчество
+            // 
+            this.Отчество.HeaderText = "Отчество";
+            this.Отчество.Name = "Отчество";
+            // 
+            // Возраст
+            // 
+            this.Возраст.HeaderText = "Возраст";
+            this.Возраст.Name = "Возраст";
+            // 
+            // Вес
+            // 
+            this.Вес.HeaderText = "Вес";
+            this.Вес.Name = "Вес";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(647, 78);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(187, 33);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1134, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Вывод);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.n);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Вывод;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label n;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Фамилия;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Имя;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Отчество;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Возраст;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Вес;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ praktika14(3-5)_Солихов/praktika14(3-5)_Солихов/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace praktika14_3_5__Солихов
+{
+    public partial class Form1 : Form
+    {
+        Queue<int> Qu = new Queue<int>();
+        Queue<string> sotrudniki = new Queue<string>();
+        Queue<string> sotrudnikist40 = new Queue<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text != "")
+                {
+                    listBox1.Items.Clear();
+                    int n = Convert.ToInt32(textBox1.Text);
+                    int p = 0;
+                    while (n > p)
+                    {
+                        p = p + 1;
+                        Qu.Enqueue(p);
+                    }
+                    listBox1.Items.Add("n = " + n);
+                    listBox1.Items.Add("Размерность очереди = " + n);
+                    listBox1.Items.Add("Верхний элеменнт очереди = " + n);
+                    listBox1.Items.Add($"Содержимое очереди = ");
+                    for (int i = Qu.Count; i > 0; i--)
+                    {
+                        listBox1.Items.Add(Qu.Dequeue());
+                    }
+                    Qu.Clear();
+                    listBox1.Items.Add("Новая размерность очереди = " + Qu.Count);
+                }
+                else
+                {
+                    MessageBox.Show("Заполните поле ввода");
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Неверный ввод");
+            }
+        }
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (File.Exists("t.txt"))
+            {
+                StreamReader si = File.OpenText("t.txt");
+                {
+                    while (true)
+                    {
+                        string st = si.ReadLine();
+                        if (st == null)
+                        {
+                            break;
+                        }
+                        string[] chelovek = st.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                        var age = chelovek.Skip(3).Take(1);
+                        foreach (string i in age)
+                        {
+                            int vozr = Convert.ToInt32(i);
+                            if (vozr < 40)
+                            {
+                                for (int j = 0; j < chelovek.Length; j++)
+                                {
+                                    sotrudniki.Enqueue(chelovek[j]);
+                                }
+                            }
+                            else
+                            {
+                                for (int j = 0; j < chelovek.Length; j++)
+                                {
+                                    sotrudnikist40.Enqueue(chelovek[j]);
+                                }
+                            }
+                        }
+                    }
+                    si.Close();
+                    int k = 0, l = 0;
+                    dataGridView1.ColumnCount = 5;
+                    dataGridView1.RowCount = ((sotrudniki.Count/5)+(sotrudnikist40.Count/5));
+                    while (sotrudniki.Count != 0)
+                    {
+                        dataGridView1.Rows[k].Cells[l].Value = sotrudniki.Dequeue();
+                        l++;
+                        if (l == 5)
+                        {
+                            l = 0;
+                            k++;
+                        }
+                    }
+                    while (sotrudnikist40.Count != 0)
+                    {
+                        dataGridView1.Rows[k].Cells[l].Value = sotrudnikist40.Dequeue();
+                        l++;
+                        if (l == 5)
+                        {
+                            l = 0;
+                            k++;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Такой файл не существует");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (File.Exists("t.txt"))
+            {
+                StreamReader si = File.OpenText("t.txt");
+                {
+                    while (true)
+                    {
+                        string st = si.ReadLine();
+                        if (st == null)
+                        {
+                            break;
+                        }
+                        string[] chelovek = st.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                        var age = chelovek.Skip(3).Take(1);
+                        foreach (string i in age)
+                        {
+                            int vozr = Convert.ToInt32(i);
+                            if (vozr < 40)
+                            {
+                                for (int j = 0; j < chelovek.Length; j++)
+                                {
+                                    sotrudniki.Enqueue(chelovek[j]);
+                                }
+                            }
+                            else
+                            {
+                                for (int j = 0; j < chelovek.Length; j++)
+                                {
+                                    sotrudnikist40.Enqueue(chelovek[j]);
+                                }
+                            }
+                        }
+                    }
+                    si.Close();
+                    int k = 0, l = 0;
+                    dataGridView1.ColumnCount = 5;
+                    dataGridView1.RowCount = ((sotrudniki.Count / 5) + (sotrudnikist40.Count / 5));
+                    while (sotrudniki.Count != 0)
+                    {
+                        dataGridView1.Rows[k].Cells[l].Value = sotrudniki.Dequeue();
+                        l++;
+                        if (l == 5)
+                        {
+                            l = 0;
+                            k++;
+                        }
+                    }
+                    while (sotrudnikist40.Count != 0)
+                    {
+                        dataGridView1.Rows[k].Cells[l].Value = sotrudnikist40.Dequeue();
+                        l++;
+                        if (l == 5)
+                        {
+                            l = 0;
+                            k++;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Такой файл не существует");
+            }
+        }
+    }
+}
